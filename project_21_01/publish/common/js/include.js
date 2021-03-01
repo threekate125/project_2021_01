f4cf48d7cabf7803fdd1cb4b72326f493e8977f4
threekate125 <66197547+threekate125@users.noreply.github.com>
2021-03-01 20:06:44 +0900

    Delete project_21_01/publish directory

--- project_21_01/publish/common/js/include.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function() {
-
-    $("#header").load("header.html");  
-    $("#main").load("mainlist.html");  
-    $("#footer").load("footer.html");  
-    $("#my_fixed").load("mypage.html");  
-    
-    });
